35be08d097b408527488e0b3a040276609d510c6
dikshyant77 <rayamajhidikshyant@gmail.com>
2024-09-08 18:59:58 +0545

    Add files via upload

---
+++ mainhomwork.py
@@ -0,0 +1,18 @@
+# ask numbers from the user
+numbers = input("square of : ")
+
+# Convert the input into integers
+numbers_list = int(input("16"))
+
+# Calculate the square of each number
+squares = [num**2 for num in numbers_list]
+
+# Output the square numbers
+print("The square numbers are:", squares)
+
+
+
+
+
+
+
